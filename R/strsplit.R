b82ef89ce0f0ad58854b0016a5320d4778566741
jelber2 <jelber2@lsu.edu>
2016-11-08 19:07:58 -0600

    added because removed from Bioconductor package genomes

---
+++ R/strsplit.R
@@ -0,0 +1,7 @@
+strsplit2 <-function(x, split=" ", n=1, ...)
+ # from Chris Stubben's genomes2 package modified by Jean Elbers
+ # https://github.com/cstubben/genomes2/blob/master/R/strsplit2.R
+{
+  y <- strsplit(as.character( x), split=split, ...)
+  sapply(y, "[", n=n)
+}
