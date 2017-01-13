4f3eba4ea65551638b1a0db53a7f2e45e30b8c63
jelber2 <jelber2@lsu.edu>
2017-01-13 11:06:07 -0600

    made create.target.region four times faster by converting GRanges object to data.frame before filtering

--- R/create.target.region.R
+++ R/create.target.region.R
@@ -6,29 +6,36 @@
 #' @examples 
 #' target.region <- create.target.region(retained.mRNA.list, gff3.filtered)
 create.target.region <- function (retained.mRNA.list, gff3.filtered) {
-  j=0
-  target.region <- ""
-
-  gff3.filtered$tags <- sub(".+Genbank:(XM_\\d+\\.\\d).+",
-                            "\\1",
-                            gff3.filtered$tags,
-                            perl=TRUE)
-
-  target.region <- gff3.filtered[0]
-
-  for (i in retained.mRNA.list)
-  {
-    j = j+1
-    target.region <- c(target.region,
-                       gff3.filtered[grepl(i,
-                                           gff3.filtered$tags),],
-                       ignore.mcols=TRUE)
-    cat("Finished filtering retained mRNA",
-        j,
-        "of",
-        length(retained.mRNA.list),
-        "finished",
-        "\n")
+  df <- data.frame(seqnames = seqnames(gff3.filtered), 
+                   starts = start(gff3.filtered),
+                   ends = end(gff3.filtered), 
+                   strands = strand(gff3.filtered),
+                   tags = gff3.filtered$tags)
+  
+  df$seqnames <- as.character(df$seqnames)
+  df$strands <- paste(df$strands)
+  df$tags <- as.character(df$tags)
+  
+  j = 0
+  target.region <- data.frame()
+  df$tags <- sub(".+Genbank:(XM_\\d+\\.\\d).+gene=(\\w+).+", 
+                 "\\1;\\2", df$tags, perl = TRUE)
+  
+  for (i in retained.mRNA.list) {
+    j = j + 1
+    target.region <- rbind(target.region, df[grepl(i,
+                                                   df$tags), ])
+    cat("Finished filtering retained mRNA", j, "of", length(retained.mRNA.list), 
+        "finished", "\n")
   }
+  target.region <- makeGRangesFromDataFrame(target.region,
+                                            keep.extra.columns=TRUE,
+                                            ignore.strand=FALSE,
+                                            seqinfo=NULL,
+                                            seqnames.field="seqnames",
+                                            start.field="starts",
+                                            end.field="ends",
+                                            strand.field="strands",
+                                            starts.in.df.are.0based=FALSE)
   return(target.region)
 }
